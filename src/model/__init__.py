9bc9b7b09deaad3cb08135e2f956b877221d9bdf
Hannibal046 <chengxin1998@stu.pku.edu.cn>
2024-05-23 06:26:51 +0000

    add sample data

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,4 +1,3 @@
-from .Tokenizer import RetrieverTokenizer,RetrieverTokenizerFast
 from .SFR import SFR
 from .xMistral import XMistralForCausalLM,XMistralConfig
 from .xMixtral import XMixtralConfig,XMixtralForCausalLM
